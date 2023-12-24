7d1b7385ce48d4720b32fb192ae8b06cbe5ae265
alilein1990 <alina.leinweber@gmx.de>
2023-12-24 16:34:15 +0100

    add fetch data from movie-api

--- dist/components/main-view/main-view.jsx
+++ dist/components/main-view/main-view.jsx
@@ -1,62 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            _id: 1,
-            Title: "The Shawshank Redemption",
-            Description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-            Genre: "Drama",
-            Director: "Frank Darabont",
-            Actors: ["Tim Robbins", "Morgan Freeman"],
-            ImagePath: "https://irs.www.warnerbros.com/gallery-jpeg/the_shawshank_redemption_posterlarge_0-675188670.jpg",
-            Featured: true
-        },
-        {
-            _id: 2,
-            Title: "Inception",
-            Description: "A thief who enters the dreams of others to steal their secrets faces a new challenge: planting an idea in a target's mind.",
-            Genre: "Sci-Fi",
-            Director: "Christopher Nolan",
-            Actors: ["Leonardo DiCaprio", "Joseph Gordon-Levitt"],
-            ImagePath: "https://i.pinimg.com/originals/ea/11/d8/ea11d8c415a8ee9f880b90b12558e717.jpg",
-            Featured: false
-        },
-        {
-            _id: 3,
-            Title: "The Godfather",
-            Description: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
-            Genre: "Crime",
-            Director: "Francis Ford Coppola",
-            Actors: ["Marlon Brando", "Al Pacino"],
-            ImagePath: "https://picfiles.alphacoders.com/137/thumb-1920-137115.jpg",
-            Featured: false
-        },
-        {
-            _id: 4,
-            Title: "The Dark Knight",
-            Description: "When the menace known as The Joker emerges, Batman must confront his greatest psychological and physical challenge.",
-            Genre: "Action",
-            Director: "Christopher Nolan",
-            Actors: ["Christian Bale", "Heath Ledger"],
-            ImagePath: "https://th.bing.com/th/id/OIP.yfBaChdkFemGi-NW6bJ-iwHaEo?rs=1&pid=ImgDetMain",
-            Featured: true
-        },
-        {
-            _id: 5,
-            Title: "Pulp Fiction",
-            Description: "The lives of two mob hitmen, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-            Genre: "Crime",
-            Director: "Quentin Tarantino",
-            Actors: ["John Travolta", "Samuel L. Jackson"],
-            ImagePath: "https://www.themoviedb.org/t/p/original/jlVOS4D6ledQGxGdL0EIte3TXfL.jpg",
-            Featured: false
-        }
-    ]);
+    const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        fetch("https://movie-api-lina-834bc70d6952.herokuapp.com/movies")
+            .then((response) => response.json())
+            .then((data) => {
+                const moviesFromApi = data.movies.map((movie) => {
+                    return {
+                        id: movie._id,
+                        Title: movie.Title,
+                        Description: movie.Description,
+                        Genre: movie.Genre,
+                        Director: movie.Director,
+                        Actors: movie.Actors,
+                        ImagePath: movie.ImagePath,
+                        Featured: movie.Featured
+                    };
+                });
+                setMovies(moviesFromApi);
+            })
+    }, []);
 
     if (selectedMovie) {
         return (
