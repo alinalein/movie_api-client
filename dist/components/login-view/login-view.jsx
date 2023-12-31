a18cfd432c7aece4cecc1105b432c818f3b10170
alilein1990 <alina.leinweber@gmx.de>
2023-12-31 16:00:58 +0100

    add logic to login-view

---
+++ dist/components/login-view/login-view.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react"
+
+export const LoginView = ({ onLoggedIn }) => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = {
+            Username: username,
+            Password: password
+        };
+
+        fetch("https://movie-api-lina-834bc70d6952.herokuapp.com/users/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        }).then((response) => response.json())
+            .then((data) => {
+                console.log("Login response: ", data);
+                if (data.user) {
+                    onLoggedIn(data.user, data.token)
+                } else {
+                    alert("No such user known")
+                }
+            })
+            .catch((e) => {
+                alert("Error occurred: ", e);
+            })
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input type="text" value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required />
+            </label>
+            <label>
+                Password:
+                <input type="password" value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    minlength="3"
+                    maxlength="20"
+                />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+    )
+}
--- dist/components/main-view/main-view.jsx
+++ dist/components/main-view/main-view.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
     const [movies, setMovies] = useState([]);
