bac9b4e13b0ad056702988b2758e99d983256493
alilein1990 <alina.leinweber@gmx.de>
2024-01-23 15:24:40 +0100

    add icon profile

--- src/components/profile-view/user-profile.jsx
+++ src/components/profile-view/user-profile.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Col, Button } from 'react-bootstrap';
 import { formatDate } from '../../utils/helpers/helpers';
 import { Link } from "react-router-dom"
-
+import { PersonBoundingBox } from "react-bootstrap-icons"
+import { PersonSquare } from "react-bootstrap-icons"
 export const UserProfile = ({ user }) => {
 
     console.log('profile:', user)
@@ -10,6 +11,7 @@ export const UserProfile = ({ user }) => {
         <div className="div_component">
             <Col className="text-center mx-auto mt-2">
                 <h2 className='mb-4 h2__text'>Your Profile</h2>
+                <PersonSquare color="white" size={180} className="mb-3" />
                 <p className="mb-3">Username: {user.Username}</p>
                 <p className="mb-3">Email: {user.Email}</p>
                 <p className="mb-4">Birthday: {formatDate(user.Birthday)}</p>
